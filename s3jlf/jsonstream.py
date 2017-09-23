0533397a5f03959e31f1b915025f364c48672e68
J C Lawrence <claw@kanga.nu>
2017-09-22 18:27:27 -0700

    lint

--- s3jlf/jsonstream.py
+++ s3jlf/jsonstream.py
@@ -1,22 +1,21 @@
 #! /usr/bin/env python
 
-import boto3, json, logtool, tempfile
+import json, logtool, tempfile
 
 @logtool.log_call
 def jsonstream (keylist, cb = None, validate = False):
-    keylist = keylist
-    s3 = boto3.resource ("s3")
-    for ndx, key in enumerate (keylist):
-      if cb is not None:
-        cb (ndx)
-      with tempfile.NamedTemporaryFile (prefix = "s3jlf_in__") as f:
-        f.write (key.get ()["Body"].read ())
-        f.seek (0)
-        for line in file (f.name): # pylint: disable=not-an-iterable
-          try:
-            if validate:
-              json.loads (line)
-            yield line
-          except ValueError:
-            continue
-    raise StopIteration
+  keylist = keylist
+  for ndx, key in enumerate (keylist):
+    if cb is not None:
+      cb (ndx)
+    with tempfile.NamedTemporaryFile (prefix = "s3jlf_in__") as f:
+      f.write (key.get ()["Body"].read ())
+      f.seek (0)
+      for line in file (f.name): # pylint: disable=not-an-iterable
+        try:
+          if validate:
+            json.loads (line)
+          yield line
+        except ValueError:
+          continue
+  raise StopIteration
